b767ff8af19d628aba016f4e2574f1bf4d14b97b
abduazim10 <abduazimit@gmail.com>
2024-03-12 15:03:13 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,6 @@
+let natija = Math.round(Math.random()*100);
+alert(`Randomda chiqqan son ${natija}`) 
+alert(`Biz buni 2ga kopaytiramiz va 2ga bolamiz`)
+alert(`2ga kopaytirganda: ${natija*2}  
+2ga bolganda: ${natija/2}
+3ga bolganda qoldigi: ${natija%3}`)
